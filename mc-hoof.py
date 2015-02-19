0d85e86d647943b0a39a439b9cdb41bffa71f610
richar <ricardosiri68@gmail.com>
2015-02-18 21:06:43 -0300

    [mc-hoof] command fix

--- mc-hoof.py
+++ mc-hoof.py
@@ -1,2 +1,8 @@
 #!/home/smoke/pyEnvs/mchoof/bin/python2
-print "hola"
+
+import os
+from mchoof.management import project
+
+
+if __name__ == '__main__':
+    project.Command(project_path=os.getcwd())
