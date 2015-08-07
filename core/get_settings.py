48b8b94b51bbfbbd6f35cf5572877523f14a7c9a
richar <ricardosiri68@gmail.com>
2015-08-07 10:42:50 -0300

    [settings] get_settings PRINTER_TEMPLATE_DIR

--- core/get_settings.py
+++ core/get_settings.py
@@ -1,11 +1,20 @@
-app_settings = __import__('main_app.settings')
+import os
+app = __import__('main_app.settings')
 
 
 def get_database_url():
 
-    return app_settings.settings.DATABASE_URL
+    return app.settings.DATABASE_URL
 
 
 def get_serial_port():
 
-    return app_settings.settings.SERIAL_PORT
+    return app.settings.SERIAL_PORT
+
+
+def get_printer_templates_dir():
+
+    return os.path.join(
+        app.settings.BASE_DIR,
+        app.settings.PRINTER_TEMPLATE_DIR
+    )
--- printer/base_escpos.py
+++ printer/base_escpos.py
@@ -1,4 +1,5 @@
-from mchoof.core.get_settings import get_serial_port
+import os
+from mchoof.core.get_settings import get_serial_port, get_printer_templates_dir
 from escpos import printer
 from mako.template import Template
 from mako.runtime import Context
@@ -28,11 +29,13 @@ class TemplateEscpos:
     def __init__(self):
 
         serial_port = get_serial_port()
+        template_dir = get_printer_templates_dir()
 
         if self.template_name:
 
+            self.template_path = os.path.join(template_dir, self.template_name)
             self.serial_escpos = SerialEscpos(devfile=serial_port)
 
     def send(self, **data):
 
-        self.serial_escpos.write_template(self.template_name, **data)
+        self.serial_escpos.write_template(self.template_path, **data)
